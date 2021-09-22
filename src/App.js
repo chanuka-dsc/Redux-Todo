8c40339bd9a53263f40563d82b16aaa1753fb997
chanuka-dsc <chanuka.dsc@gmail.com>
2021-09-22 22:08:42 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Todo list app</header>
+      <header className="App-header">
+        <h2>Redux-based Todo app</h2>
+        <form>
+          <input className="todo-in" placeholder="Enter a todo" />
+          <button className="todo-btn" type="submit">
+            Go
+          </button>
+        </form>
+        <ul className="all-todo">
+          <li className="single-todo">
+            <span className="todo-text">First todo</span>
+            <button className="btn-del">delete</button>
+          </li>
+        </ul>
+      </header>
     </div>
   );
 }
